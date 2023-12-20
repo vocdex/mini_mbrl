7d9f6b38d7eb909c295a2e5ef512fa69108c8760
Shukrullo Nazirjonov <nazirjonovsh2000@gmail.com>
2023-12-20 22:51:03 +0100

    added OpenLoopPolicy

--- mini_mbrl/utils.py
+++ mini_mbrl/utils.py
@@ -10,18 +10,29 @@ class RandomPolicy:
         return self.env.action_space.sample()
 
 
-def sample_trajectory(env, policy, max_length, is_deterministic: bool, seed: int) -> [str, np.ndarray]:
+class OpenLoopPolicy:
+    """This should return an action when called with an observation."""
+
+    def __init__(self, action_sequence, env: gym.Env):
+        self.action_sequence = action_sequence
+        self.env = env
+
+    def get_action(self, obs, state, **kwargs):
+        return self.action_sequence[0]
+
+
+def sample_trajectory(env, policy, n_steps, is_deterministic: bool, seed: int) -> [str, np.ndarray]:
     """Samples a single trajectory using the given policy."""
     if is_deterministic:
         ob, _ = env.reset(seed=seed)
         env.action_space.seed(seed)
     else:
-        ob, _ = env.reset(None)
+        ob, _ = env.reset()
     (
         obs,
+        next_obs,
         acs,
         rewards,
-        next_obs,
         dones,
     ) = (
         [],
@@ -31,7 +42,7 @@ def sample_trajectory(env, policy, max_length, is_deterministic: bool, seed: int
         [],
     )
     step = 0
-    while True and step < max_length:
+    while True and step < n_steps:
         obs.append(ob)
         action = policy(ob)
         acs.append(action)
@@ -44,35 +55,34 @@ def sample_trajectory(env, policy, max_length, is_deterministic: bool, seed: int
         step += 1
 
     env.close()
-    traj_stats = {"total_reward": np.sum(rewards), "length": len(rewards)}
+    traj_stats = {"total_reward": np.sum(rewards), "length": step}
     return {
         "observations": np.array(obs),
+        "next_observations": np.array(next_obs),
         "actions": np.array(acs),
         "rewards": np.array(rewards),
-        "next_observations": np.array(next_obs),
         "dones": np.array(dones),
-        "stats": traj_stats,
+        "traj_stats": traj_stats,
     }
 
 
-def sample_trajectories(
-    env: gym.Env, policy, num_traj: int, max_length: int, is_deterministic: bool
+def sample_n_trajectories(
+    env: gym.Env, policy, num_traj: int, n_steps: int, is_deterministic: bool
 ) -> [str, np.ndarray]:
     """Samples a batch of trajectories using the given policy."""
     trajs = []
     # use different seeds for each trajectory
     for i in range(num_traj):
-        traj = sample_trajectory(env, policy, max_length, is_deterministic, seed=i)
+        traj = sample_trajectory(env, policy, n_steps, is_deterministic, seed=i)
         trajs.append(traj)
     return trajs
 
 
-# if __name__ == "__main__":
-#     env = gym.make("CartPole-v1")
-#     policy = RandomPolicy(env)
-#     trajs = sample_trajectories(env, policy, 2, 100, True)
+if __name__ == "__main__":
+    env = gym.make("CartPole-v1")
+    policy = RandomPolicy(env)
+    trajs = sample_n_trajectories(env, policy, 10, 100, False)
 
-#     for traj in trajs:
-#         obs = traj["observations"]
-#         mean_obs = np.mean(obs, axis=0)
-#         print("Mean observation: ", mean_obs)
+    for traj in trajs:
+        print("Length of trajectory: ", len(traj["observations"]))
+        print("Total reward: ", traj["traj_stats"]["total_reward"])
