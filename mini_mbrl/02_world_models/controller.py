9e229ec0f414935e2e2f6b0a83720f30707bedbf
Shukrullo Nazirjonov <nazirjonovsh2000@gmail.com>
2024-10-20 10:26:56 +0200

    add controller

---
+++ mini_mbrl/02_world_models/controller.py
@@ -0,0 +1,15 @@
+"""Simple controller model for the world models agent."""
+import torch
+import torch.nn as nn
+
+
+class Controller(nn.Module):
+    """Controller"""
+
+    def __init__(self, latents, recurrents, actions):
+        super().__init__()
+        self.fc = nn.Linear(latents + recurrents, actions)
+
+    def forward(self, *inputs):
+        cat_in = torch.cat(inputs, dim=1)
+        return self.fc(cat_in)
